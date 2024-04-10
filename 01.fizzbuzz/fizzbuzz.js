96fed76aa780b288dfe7a26da5b91a76ea470392
icchankun <ikkun.official@gmail.com>
2024-04-10 17:58:23 +0900

    JavaScriptを使って、FizzBuzzのプログラムを書く。

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const DISPLAY_COUNT = 20;
+const fizzbuzz = (num) => {
+  if (num % 15 == 0) console.log("FizzBuzz");
+  else if (num % 3 == 0) console.log("Fizz");
+  else if (num % 5 == 0) console.log("Buzz");
+  else console.log(String(num));
+};
+
+Array.from(Array(DISPLAY_COUNT).keys(), (x) => x + 1).forEach((num) =>
+  fizzbuzz(num),
+);
