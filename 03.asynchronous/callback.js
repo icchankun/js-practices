3c6f410036dddbbea790f80d604be762d9c3f472
icchankun <ikkun.official@gmail.com>
2024-05-04 23:29:40 +0900

    コールバック形式で要件のプログラムを書く。

---
+++ 03.asynchronous/callback.js
@@ -0,0 +1,34 @@
+import sqlite3 from "sqlite3";
+import timers from "timers/promises";
+const db = new sqlite3.Database(":memory:");
+
+// エラーなしのプログラム
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    db.run("INSERT INTO books (title) VALUES (?)", "Book Title", function () {
+      console.log("id: " + this.lastID);
+      db.get("SELECT * FROM books", (_, row) => {
+        console.log(row.id + ": " + row.title);
+        db.run("DROP TABLE books");
+      });
+    });
+  },
+);
+
+// エラーなしとエラーありのプログラムを正しく続けて実行させるため、0.1秒待機する。
+await timers.setTimeout(100);
+
+// エラーありのプログラム
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    db.run("INSERT INTO books (author) VALUES (?)", "Book Author", (err) => {
+      console.error(err.message);
+      db.get("SELECT author FROM books", (err) => {
+        console.error(err.message);
+        db.run("DROP TABLE books");
+      });
+    });
+  },
+);
