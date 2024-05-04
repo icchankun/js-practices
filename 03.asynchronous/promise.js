151b744d3af3e61c467ded95d9af6339677b1310
icchankun <ikkun.official@gmail.com>
2024-05-04 23:45:01 +0900

    Promiseとthen、catchを使う形式で要件のプログラムを書く。

---
+++ 03.asynchronous/promise.js
@@ -0,0 +1,64 @@
+import sqlite3 from "sqlite3";
+import timers from "timers/promises";
+const db = new sqlite3.Database(":memory:");
+
+const asyncRun = (db, sql, params = []) => {
+  return new Promise((resolve, reject) => {
+    db.run(sql, params, function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(this);
+      }
+    });
+  });
+};
+
+const asyncGet = (db, sql, params = []) => {
+  return new Promise((resolve, reject) => {
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
+      }
+    });
+  });
+};
+
+// エラーなしのプログラム
+asyncRun(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() => {
+    return asyncRun(db, "INSERT INTO books (title) VALUES (?)", "Book Title");
+  })
+  .then((table) => {
+    console.log("id: " + table.lastID);
+    return asyncGet(db, "SELECT * FROM books");
+  })
+  .then((row) => {
+    console.log(row.id + ": " + row.title);
+    asyncRun(db, "DROP TABLE books");
+  });
+
+// エラーなしとエラーありのプログラムを正しく続けて実行させるため、0.1秒待機する。
+await timers.setTimeout(100);
+
+// エラーありのプログラム
+asyncRun(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() => {
+    return asyncRun(db, "INSERT INTO books (author) VALUES (?)", "Book Author");
+  })
+  .catch((err) => {
+    console.error(err.message);
+    return asyncGet(db, "SELECT author FROM books");
+  })
+  .catch((err) => {
+    console.error(err.message);
+    asyncRun(db, "DROP TABLE books");
+  });
