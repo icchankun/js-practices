0b0e77b10df5da9a16641dbebadc4c36b99aa32c
icchankun <ikkun.official@gmail.com>
2024-05-07 17:36:55 +0900

    即時実行関数ではなく Top-level await を使う。

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -1,68 +1,60 @@
-import timers from "timers/promises";
 import sqlite3 from "sqlite3";
 import { asyncRun, asyncGet } from "./async_methods.js";
 
 const db = new sqlite3.Database(":memory:");
 
 // エラーなしのプログラム
-(async () => {
-  await asyncRun(
+await asyncRun(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+
+try {
+  const statement = await asyncRun(
     db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+    "INSERT INTO books (title) VALUES (?)",
+    "Book Title",
   );
+  console.log("id: " + statement.lastID);
+} catch (err) {
+  console.error(err.message);
+}
 
-  try {
-    const statement = await asyncRun(
-      db,
-      "INSERT INTO books (title) VALUES (?)",
-      "Book Title",
-    );
-    console.log("id: " + statement.lastID);
-  } catch (err) {
-    console.error(err.message);
-  }
+try {
+  const row = await asyncGet(db, "SELECT * FROM books");
+  console.log(row.id + ": " + row.title);
+} catch (err) {
+  console.error(err.message);
+}
 
-  try {
-    const row = await asyncGet(db, "SELECT * FROM books");
-    console.log(row.id + ": " + row.title);
-  } catch (err) {
-    console.error(err.message);
-  }
-
-  asyncRun(db, "DROP TABLE books");
-})();
-
-// エラーなしとエラーありのプログラムを正しく続けて実行させるため、0.1秒待機する。
-await timers.setTimeout(100);
+await asyncRun(db, "DROP TABLE books");
 
 // エラーありのプログラム
-(async () => {
-  await asyncRun(
+await asyncRun(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+
+try {
+  const statement = await asyncRun(
     db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+    "INSERT INTO books (author) VALUES (?)",
+    "Book Author",
   );
-
-  try {
-    const statement = await asyncRun(
-      db,
-      "INSERT INTO books (author) VALUES (?)",
-      "Book Author",
-    );
-    console.log("id: " + statement.lastID);
-  } catch (err) {
-    if (err.code === "SQLITE_ERROR") {
-      console.error(err.message);
-    }
+  console.log("id: " + statement.lastID);
+} catch (err) {
+  if (err.code === "SQLITE_ERROR") {
+    console.error(err.message);
   }
+}
 
-  try {
-    const row = await asyncGet(db, "SELECT author FROM books");
-    console.log(row.id + ": " + row.title);
-  } catch (err) {
-    if (err.code === "SQLITE_ERROR") {
-      console.error(err.message);
-    }
+try {
+  const row = await asyncGet(db, "SELECT author FROM books");
+  console.log(row.id + ": " + row.title);
+} catch (err) {
+  if (err.code === "SQLITE_ERROR") {
+    console.error(err.message);
   }
+}
 
-  asyncRun(db, "DROP TABLE books");
-})();
+await asyncRun(db, "DROP TABLE books");
