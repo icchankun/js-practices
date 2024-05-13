fd07a119c97997d1551f82c61b7fbd3e3031aa47
icchankun <ikkun.official@gmail.com>
2024-05-13 17:16:42 +0900

    アプリを作る上で使うnode-sqlite3のAPIを非同期関数として定義し直し、モジュールとする。

---
+++ 04.class/lib/async-sqlite3-functions.js
@@ -0,0 +1,25 @@
+const asyncRun = (db, sql, params = []) => {
+  return new Promise((resolve, reject) => {
+    db.run(sql, params, function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(this);
+      }
+    });
+  });
+};
+
+const asyncAll = (db, sql, params = []) => {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+};
+
+export { asyncRun, asyncAll };
